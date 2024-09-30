b890e1c230abe4d7815812835583027ba3422729
AminaAlawadhi <amina7alawadhi@gmail.com>
2024-09-30 11:13:17 +0300

    initial commit

---
+++ main.dart
@@ -0,0 +1,12 @@
+//Create some variables to define your `firstName`, `lastName`, `age`, `height`.
+//Then, print these variables.
+void main() {
+  String firstname = "Amina";
+  String lastname = "Alawadhi";
+  int age = 23;
+  double height = 1.67;
+  print(firstname);
+  print(lastname);
+  print(age);
+  print(height);
+}
